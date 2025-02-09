17dfcc9358b98852601d8bd56c7072e96affc7b0
Enzo <aypierre30@gmail.com>
2025-02-09 15:56:02 +0100

    feat: signin & signup

---
+++ src/app/screens/auth/Signin.tsx
@@ -0,0 +1,84 @@
+import { Button } from '@/components/common/ButtonV2';
+import { Input } from '@/components/common/Input';
+import Page from '@/components/common/Page';
+import useAuth from '@/hooks/useAuth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useNavigation } from '@react-navigation/native';
+import { ArrowLeft } from 'lucide-react-native';
+import React, { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { Text, type TextInput, View } from 'react-native';
+import { z } from 'zod';
+
+const loginSchema = z.object({
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+});
+
+export const Signin = () => {
+    const navigation = useNavigation();
+    const { login, isLoading } = useAuth();
+    const { t } = useTranslation();
+
+    const passwordRef = useRef<TextInput>(null);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    });
+
+    const handleLogin = async (data) => {
+        try {
+            await login(data.email, data.password);
+        } catch (err) {
+            console.error('Invalid email or password');
+        }
+    };
+
+    return (
+        <Page className="flex flex-col gap-8">
+            <ArrowLeft color="white" onPress={() => navigation.goBack()} />
+            <Text className="h1">{t('auth.signIn')}</Text>
+            <View className="flex flex-col gap-10">
+                <Input
+                    placeholder="christophe.lerouge@imt-atlantique.net"
+                    control={control}
+                    name="email"
+                    label={t('auth.email')}
+                    returnKeyType='next'
+                    onSubmitEditing={() => passwordRef.current?.focus()}
+                />
+                <Input
+                    placeholder="••••••••••"
+                    control={control}
+                    name="password"
+                    label={t('auth.password')}
+                    secureTextEntry
+                    ref={passwordRef}
+                    returnKeyType='done'
+                    onSubmitEditing={handleSubmit(handleLogin)}
+                    error={errors.password?.message}
+                />
+                <View className='flex flex-col gap-2'>
+                    <Button label={t('auth.signIn')} onPress={handleSubmit(handleLogin)} disabled={isLoading} />
+                    <Button
+                        label={t('auth.noAccount')}
+                        onPress={() => navigation.navigate('Auth', { screen: 'Signup',  })}
+                        disabled={isLoading}
+                        variant="link"
+                    />
+                </View>
+            </View>
+        </Page>
+    );
+};
+
+export default Signin;
---
+++ src/app/screens/auth/Signup.tsx
@@ -0,0 +1,129 @@
+import { Input } from '@/components/common/Input';
+import Page from '@/components/common/Page';
+import { useAuth } from '@/hooks/useAuth';
+import { t } from 'i18next';
+import { ArrowLeft } from 'lucide-react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, type TextInput } from 'react-native';
+import { Button } from '@/components/common/ButtonV2';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useRef } from 'react';
+
+const signupSchema = z
+    .object({
+        firstName: z.string().min(1, t('auth.errors.firstName')),
+        lastName: z.string().min(1, t('auth.errors.lastName')),
+        email: z.string().email(t('auth.errors.email')),
+        password: z.string().min(6, t('auth.errors.password')),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: t('auth.errors.confirmPassword'),
+        path: ['confirmPassword'],
+    });
+
+export const Signup = () => {
+    const { register } = useAuth();
+    const navigation = useNavigation();
+
+    const lastNameRef = useRef<TextInput>(null);
+    const emailRef = useRef<TextInput>(null);
+    const passwordRef = useRef<TextInput>(null);
+    const confirmPasswordRef = useRef<TextInput>(null);
+
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(signupSchema),
+        defaultValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+        },
+    });
+
+    const onSubmit = async (data) => {
+        try {
+            await register(data.email, data.password);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <Page className="flex flex-col gap-8">
+            <ArrowLeft color="white" onPress={() => navigation.goBack()} />
+            <Text className="h1">{t('auth.signUp')}</Text>
+
+            <View className="flex flex-col gap-6">
+                <Input
+                    placeholder="Christophe"
+                    label={t('auth.firstName')}
+                    control={control}
+                    name="firstName"
+                    error={errors.firstName?.message}
+                    returnKeyType="next"
+                    onSubmitEditing={() => lastNameRef.current?.focus()}
+                />
+                <Input
+                    placeholder="Le Rouge"
+                    label={t('auth.lastName')}
+                    control={control}
+                    name="lastName"
+                    error={errors.lastName?.message}
+                    returnKeyType="next"
+                    ref={lastNameRef}
+                    onSubmitEditing={() => emailRef.current?.focus()}
+                />
+                <Input
+                    placeholder="christophe.lerouge@imt-atlantique.net"
+                    label={t('auth.email')}
+                    control={control}
+                    name="email"
+                    error={errors.email?.message}
+                    returnKeyType="next"
+                    ref={emailRef}
+                    onSubmitEditing={() => passwordRef.current?.focus()}
+                />
+                <Input
+                    placeholder="••••••••••"
+                    label={t('auth.password')}
+                    control={control}
+                    name="password"
+                    secureTextEntry
+                    error={errors.password?.message}
+                    returnKeyType="next"
+                    ref={passwordRef}
+                    onSubmitEditing={() => confirmPasswordRef.current?.focus()}
+                />
+                <Input
+                    placeholder="••••••••••"
+                    label={t('auth.confirmPassword')}
+                    control={control}
+                    name="confirmPassword"
+                    secureTextEntry
+                    error={errors.confirmPassword?.message}
+                    returnKeyType="done"
+                    ref={confirmPasswordRef}
+                    onSubmitEditing={handleSubmit(onSubmit)}
+                />
+            </View>
+            <View className="flex flex-col gap-2">
+                <Button label={t('auth.createAccount')} onPress={handleSubmit(onSubmit)} />
+                <Button
+                    label={t('auth.gotAccount')}
+                    onPress={() => navigation.navigate('Auth', { screen: 'Signin' })}
+                    variant="link"
+                />
+            </View>
+        </Page>
+    );
+};
+
+export default Signup;

caec7ffb4c1fdc0700fc9773225d1b10b215d86e
Yohann <73314529+yohann69@users.noreply.github.com>
2025-04-07 14:58:31 +0200

    Revert "config est plus reliable"
    This reverts commit 0cc79b5f1b82c3ee433c3d48c781e78fafec32d9.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,21 +1,5 @@
-import { z } from "zod";
-
-const configSchema = z.object({
-  EXPO_PUBLIC_WEATHER_API_KEY: z.string(),
-  EXPO_PUBLIC_API_URL: z.string(),
-  EXPO_PUBLIC_API_URL_DEV: z.string().optional(),
-});
-
-const config = configSchema.parse(process.env);
-
-if (!config) {
-  throw new Error("Config is not set");
-}
-
-const {
-  EXPO_PUBLIC_WEATHER_API_KEY: weatherApiKey,
-  EXPO_PUBLIC_API_URL: apiUrlProd,
-  EXPO_PUBLIC_API_URL_DEV: apiUrlDev,
-} = config;
+const weatherApiKey = process.env.EXPO_PUBLIC_WEATHER_API_KEY;
+const apiUrlProd = process.env.EXPO_PUBLIC_API_URL;
+const apiUrlDev = process.env.EXPO_PUBLIC_API_URL_DEV;
 
 export { weatherApiKey, apiUrlProd, apiUrlDev };

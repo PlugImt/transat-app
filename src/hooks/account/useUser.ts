134aecb55ea5006b2f07729aac57d136bd07c16c
Lucie Delestre <me@luclu7.fr>
2025-05-06 23:24:06 +0200

    staleTime sur useUser

--- src/hooks/account/useUser.ts
+++ src/hooks/account/useUser.ts
@@ -6,6 +6,8 @@ export const useUser = () => {
   const { data, isPending, error, isError } = useQuery({
     queryKey: QUERY_KEYS.user,
     queryFn: () => fetchUser(),
+    refetchOnWindowFocus: false,
+    staleTime: 1000 * 60 * 5, // 5 minutes
   });
 
   return { data, isPending, error, isError };

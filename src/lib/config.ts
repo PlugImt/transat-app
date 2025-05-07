00c007f275d471806411eb42867c085e7fa2d6c4
Lucie Delestre <me@luclu7.fr>
2025-05-08 00:25:09 +0200

    Weather sur le serveur

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,7 +1,6 @@
-const weatherApiKey = process.env.EXPO_PUBLIC_WEATHER_API_KEY || "";
 const apiUrlProd = process.env.EXPO_PUBLIC_API_URL || "";
 const apiUrlDev = process.env.EXPO_PUBLIC_API_URL_DEV || "";
 const translationApiKey =
   process.env.EXPO_PUBLIC_GOOGLE_TRANSLATE_API_KEY || "";
 
-export { weatherApiKey, apiUrlProd, apiUrlDev, translationApiKey };
+export { apiUrlProd, apiUrlDev, translationApiKey };
--- src/lib/weather.ts
+++ src/lib/weather.ts
@@ -1,44 +1,16 @@
 import i18n from "@/i18n";
-import type { WeatherData } from "@/types/weather";
-import { t } from "i18next";
-import { weatherApiKey } from "./config";
-import { translateText } from "./translation";
+import type { WeatherData, WeatherDataApi } from "@/types/weather";
+import { apiRequest } from "./apiRequest";
 
 export async function fetchWeather(): Promise<WeatherData> {
-  const lat = 47.218371; // Latitude de Nantes
-  const lon = -1.553621; // Longitude de Nantes
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${weatherApiKey}`;
-
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(t("common.errors.unableToFetch"));
-  }
-  const data = await response.json();
-
-  const temperature: number = data.main.temp;
-  const weatherCondition: string = data.weather[0].main;
-  const img: string = data.weather[0].icon;
-
-  // Get the current language from i18n
   const currentLanguage = i18n.language.toUpperCase();
-  let translatedCondition: string = weatherCondition;
-
-  if (currentLanguage !== "EN") {
-    // Translate the weather condition
-    try {
-      translatedCondition = await translateText(
-        weatherCondition,
-        // biome-ignore lint/suspicious/noExplicitAny: type DeepLSupportedLanguages which is string
-        currentLanguage.toLowerCase() as any,
-      );
-    } catch (error) {
-      console.error("Error translating weather condition", error);
-    }
-  }
+  const data = await apiRequest<WeatherDataApi>(
+    `/api/weather?language=${currentLanguage}`,
+  );
 
   return {
-    temperature,
-    condition: translatedCondition,
-    img,
+    temperature: data.main.temp,
+    condition: data.weather[0].main,
+    img: data.weather[0].icon,
   } satisfies WeatherData;
 }

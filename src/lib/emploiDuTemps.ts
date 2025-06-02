ccf886279f435e2ceebc8b00da49e9ba0a249484
Pacôme CAILLETEAU <pacomecailleteau@gmail.com>
2025-06-02 18:33:22 +0200

    Ajout du service Emploi du temps

--- src/lib/emploiDuTemps.ts
+++ src/lib/emploiDuTemps.ts
@@ -1,18 +1,17 @@
 import type { EmploiDuTempsData } from "@/types/emploiDuTemps";
-import type { MenuData } from "@/types/restaurant";
 import i18n from "i18next";
 import { apiRequest } from "./apiRequest";
 
 const TARGET_URL = "/api/emploiDuTemps";
 
-export async function getEmploiDuTemps(): Promise<MenuData | undefined> {
+export async function getEmploiDuTemps(): Promise<EmploiDuTempsData | undefined> {
   // retrieve the language from the local storage
   const currentLanguage = i18n.language.toLowerCase();
 
   const queryParams = new URLSearchParams();
   queryParams.append("language", currentLanguage);
 
-  return await apiRequest<MenuData>(
+  return await apiRequest<EmploiDuTempsData>(
     `${TARGET_URL}?${queryParams.toString()}`,
     "GET",
   );

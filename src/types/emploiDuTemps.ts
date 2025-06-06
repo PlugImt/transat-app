58dcdfc9ab9f187f5a51e0bf95a0054aba6c425c
Pacôme CAILLETEAU <pacomecailleteau@gmail.com>
2025-06-06 11:10:25 +0200

    type emploidutemps et update type user

--- src/types/emploiDuTemps.ts
+++ src/types/emploiDuTemps.ts
@@ -1,3 +1,14 @@
+import type { User } from "@/types/user";
+
 export interface EmploiDuTempsData {
-  updated_date: string;
+  id: number;
+  date: Date;
+  titre: string;
+  heure_debut: string;
+  heure_fin: string;
+  profs: string;
+  salles: string;
+  groupe: string;
+  user_email: User["email"];
+  created_at: string;
 }

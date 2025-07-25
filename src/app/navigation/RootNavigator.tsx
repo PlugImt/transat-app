e1ff3dcdc0e325870d2032a29c29c81f1dd33b24
Enzo <aypierre30@gmail.com>
2025-07-25 22:11:37 +0200

    feat: ajout d'un écran de chargement tant que l'application n'est pas prête

--- src/app/navigation/RootNavigator.tsx
+++ src/app/navigation/RootNavigator.tsx
@@ -2,7 +2,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { useEffect, useState } from "react";
 import { AppNavigator } from "@/app/navigation/AppNavigator";
 import { AuthNavigator } from "@/app/navigation/AuthNavigator";
-import { HomeLoading } from "@/app/screens/home/Home";
+import { SplashScreen } from "@/components/animations/SplashScreen";
 import { useAuth } from "@/hooks/account/useAuth";
 import { usePushNotifications } from "@/hooks/home";
 import { i18nInitializedPromise } from "@/i18n";
@@ -23,9 +23,9 @@ export const RootNavigator = () => {
     });
   }, []);
 
-  // pendant qu'on vérifie si l'utilisateur est connecté et que l'i18n est prêt, on affiche un écran de chargement
+  // Pendant qu'on vérifie si l'utilisateur est connecté et que l'i18n est prêt, on affiche un écran de chargement
   if (user === undefined || !isI18nReady) {
-    return <HomeLoading />;
+    return <SplashScreen />;
   }
 
   return (

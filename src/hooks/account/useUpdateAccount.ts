0006c1b0d156f52d2ecbeaf3a2b155a1ba1c939c
Yohann <73314529+yohann69@users.noreply.github.com>
2025-08-13 01:00:27 +0200

    fix: auto update account after profile update

--- src/hooks/account/useUpdateAccount.ts
+++ src/hooks/account/useUpdateAccount.ts
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { updateUser } from "@/api";
+import { fetchUser, updateUser } from "@/api";
 import { QUERY_KEYS } from "@/constants";
 import { storage } from "@/services/storage/asyncStorage";
 
@@ -10,8 +10,12 @@ export const useUpdateAccount = () => {
     mutationFn: updateUser,
     onSuccess: async (updatedUser) => {
       await storage.set("newf", updatedUser);
-      queryClient.setQueryData(QUERY_KEYS.user, updatedUser);
-      queryClient.setQueryData(QUERY_KEYS.auth.user, updatedUser);
+
+      const freshUser = await fetchUser();
+      await storage.set("newf", freshUser);
+
+      queryClient.setQueryData(QUERY_KEYS.user, freshUser);
+      queryClient.setQueryData(QUERY_KEYS.auth.user, freshUser);
       await queryClient.invalidateQueries({ queryKey: QUERY_KEYS.user });
       await queryClient.invalidateQueries({ queryKey: QUERY_KEYS.auth.user });
     },

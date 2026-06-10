f31309cba4f60a07d2ef72fe5ac73d33058eda0e
Célie Ponroy <117756005+celie-ponroy@users.noreply.github.com>
2026-06-10 16:31:13 +0200

    Update Navbar.tsx

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from "expo-router/build/react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "expo-router/build/react-navigation/native-stack";
-import { LucideHome, Play, User,Wrench } from "lucide-react-native";
+import { LucideHome, Play, User, Wrench } from "lucide-react-native";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "@/contexts/ThemeContext";
 import { screenOptions, tabBarOptions } from "@/navigation/navigationConfig";

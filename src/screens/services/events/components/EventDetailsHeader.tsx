3e417cfce9ecec9b6421c40c69d6e90186004a89
Célie <celie.ponroy@laposte.net>
2026-06-15 15:49:01 +0200

    v fonctionnelle  + ajout linking

--- src/screens/services/events/components/EventDetailsHeader.tsx
+++ src/screens/services/events/components/EventDetailsHeader.tsx
@@ -6,11 +6,8 @@ import {
   ExternalLink,
   MapPin,
 } from "lucide-react-native";
-import { Calendar as CalendarIcon } from "lucide-react-native";
 import { useTranslation } from "react-i18next";
-import { Alert, Linking, View } from "react-native";
-import * as Calendar from "expo-calendar";
-import { useState } from "react";
+import { Linking, View } from "react-native";
 import { Button } from "@/components/common/Button";
 import Image from "@/components/common/Image";
 import { Text } from "@/components/common/Text";
@@ -23,8 +20,9 @@ import {
   useLeaveClubMutation,
 } from "@/hooks/services/event/useEvent";
 import type { EventDetailsRouteProp } from "./EventDetails";
-import { Platform } from 'react-native';
-import BottomSheet from "@gorhom/bottom-sheet";
+import React from "react";
+import { ExportButton } from "./ExportEvent";
+
 
 interface NotificationButtonProps {
   isMember: boolean;
@@ -62,94 +60,6 @@ const NotificationButton = ({
       className="flex-1"
       disabled={disabled}
       isUpdating={isJoining || isLeaving}
-    />
-  );
-};
-
-interface ExportButtonProps {
-  event: EventDetails;
-  disabled?: boolean;
-}
-
-const ExportButton = ({ event, disabled }: ExportButtonProps) => {
-  const { t } = useTranslation();
-  const [isExporting, setIsExporting] = useState(false);
-
-const exportToCalendar = async () => {
-  setIsExporting(true);
-
-  try {
-    const { status } = await Calendar.requestCalendarPermissions(); 
-    if (status !== "granted") {
-      Alert.alert(
-        t("permissionsRequired"),
-        t("enableInSettings"),
-        [
-          { text: t("cancel"), style: "cancel" },
-          { text: t("openSettings"), onPress: () => Linking.openSettings() },
-        ]
-      );
-      return;
-    }
-     
-    let choosedCalendar;
-    if(Platform.OS === 'ios') {
-      choosedCalendar = await Calendar.presentPicker();
-    }else if(Platform.OS === 'android') {
-     let calendars = await Calendar.getCalendars();
-      console.log(calendars[0]);
-      //let the user choose a calendar
-      
-    }
-    if(!choosedCalendar) {
-      Alert.alert(t("error"), t("noCalendar"));
-      return;
-    }
-    
-    const existingEvents = await choosedCalendar.listEvents(
-      new Date(event.start_date),
-      event.end_date ? new Date(event.end_date) : new Date(event.start_date)
-    );
-
-    const duplicateEvent = existingEvents.find(
-      (e) => e.title === event.name && e.startDate.toLocaleString() === new Date(event.start_date).toLocaleString()
-    );
-
-    if (duplicateEvent) {
-      Alert.alert(t("error"), t("Duplicate"));
-      return;
-    }
-  
-    choosedCalendar.createEvent( {
-      title: event.name,
-      startDate: new Date(event.start_date),
-      endDate: event.end_date ? new Date(event.end_date) : undefined,
-      location: event.location,
-      notes: event.description,
-      url: event.link,
-    }).then(() => {
-      Alert.alert(t("success"), t("services.events.export.success"));
-    }).catch((error) => {
-      console.error("Event creation error:", error);
-      Alert.alert(t("error"), t("services.events.export.failed"));
-    });
-
-  } catch (error) {
-    console.error("Export error:", error);
-    Alert.alert(t("error"), t("services.events.export.failed"));
-  } finally {
-    setIsExporting(false);
-  }
-};
-
-  return (
-    <Button
-      label={t("export")}
-      onPress={exportToCalendar}
-      icon={<CalendarIcon size={16} />}
-      disabled={disabled}
-      isLoading={isExporting}
-      className="w-full"
     />
   );
 };

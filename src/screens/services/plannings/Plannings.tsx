9ba71b9304ba5a3950c57bd3518c126c82f6f675
Akinimaginable <akn@etrange.org>
2026-06-19 10:42:37 +0200

    feat: Add PlanningInte screen and update navigation

--- src/screens/services/plannings/Plannings.tsx
+++ src/screens/services/plannings/Plannings.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import LinkCard from "@/components/custom/card/LinkCard";
 import { Page } from "@/components/page/Page";
 import { useNavigation } from "expo-router/react-navigation";
@@ -6,15 +7,23 @@ import { t } from "i18next";
 
 export const Plannings = () => {
   const navigation = useNavigation<AppNavigation>();
-  
+
   return (
     <Page>
-      <LinkCard
-        title={t('services.plannings.sport.title')}
-        description={t('services.plannings.sport.description')}
-        size={"default"}
-        onPress={()=>{navigation.navigate("PlanningSport")}}
-    />
+      <>
+        <LinkCard
+          title={t('services.plannings.sport.title')}
+          description={t('services.plannings.sport.description')}
+          size={"default"}
+          onPress={() => { navigation.navigate("PlanningSport") }}
+        />
+        <LinkCard
+          title="inté"
+          description="inté"
+          size={"default"}
+          onPress={() => { navigation.navigate("PlanningInte") }}
+        />
+      </>
     </Page>
   );
 };
